a25d226ab76c2dec877fd878cf7ed17d501fa98b
valerie2107 <kinkamyl@hotmail.com>
2023-04-06 15:43:03 +0200

    encore

--- view/public_view/include/search.inc.php
+++ view/public_view/include/search.inc.php
@@ -1,12 +1,23 @@
 <?php
 
 $item = $db->query('SELECT instruments FROM categorie ORDER BY id DESC');   /*$db défini dans index */
-$item = $db->query('SELECT instruments FROM categorie WHERE instruments LIKE "%google%" ORDER BY id DESC');
+$item = $db->query('SELECT instruments FROM categorie WHERE instruments LIKE "%' .$google. '%" ORDER BY id DESC');
+
+$item = $db->query('SELECT musiciens FROM categorie ORDER BY id DESC');   /*$db défini dans index */
+$item = $db->query('SELECT musiciens FROM categorie WHERE musiciens LIKE "%' .$google. '%"  ORDER BY id DESC');
+
+$item = $db->query('SELECT instruments FROM musiciens ORDER BY id DESC');   /*$db défini dans index */
+$item = $db->query('SELECT instruments FROM musiciens WHERE instruments LIKE "%' .$google. '%"  ORDER BY id DESC');
+
+$item = $db->query('SELECT musiciens FROM instruments ORDER BY id DESC');   /*$db défini dans index */
+$item = $db->query('SELECT musiciens FROM instruments WHERE musiciens LIKE "%' .$google. '%"  ORDER BY id DESC');
+
+
 
 if(isset($_GET['google']) AND !empty($_GET['google'])){
     $google = htmlspecialchars($_GET['google']);
 
-    $i
+    $i;
 }
 
 ?>
