c044c170a221f5584f4a4719373d0fc6cd9a161e
valerie2107 <kinkamyl@hotmail.com>
2023-04-06 15:12:55 +0200

    avancement

--- view/public_view/include/search.inc.php
+++ view/public_view/include/search.inc.php
@@ -1,16 +1,24 @@
 <?php
 
-$item = $db->query('SELECT titre FROM articles ORDER BY id DESC');   /*$db défini dans index */
-$item = $db->query('SELECT titre FROM articles WHERE titre LIKE "%q%" ORDER BY id DESC');
+$item = $db->query('SELECT instruments FROM categorie ORDER BY id DESC');   /*$db défini dans index */
+$item = $db->query('SELECT instruments FROM categorie WHERE instruments LIKE "%google%" ORDER BY id DESC');
+
+if(isset($_GET['google']) AND !empty($_GET['google'])){
+    $google = htmlspecialchars($_GET['google']);
+
+    $i
+}
 
 ?>
+
+
 <form method="GET" >
-    <input type="search" name="loupe" placeholder="votre recherche:">
+    <input type="search" name="google" placeholder="votre recherche:">
     <input type="submit" value="Valider">
 </form>
 
 <ul>
     <?php while($i = $item->fetch()){ ?>
-        <li><?= $i['titre'] ?> </li>
+        <li><?= $i['instruments'] ?> </li>
 <?php }  ?>
 </ul>
