82318febd8e9affb6fd10d2bd1a2d1ff0885eef3
Slashyo <tom.bik@hotmail.be>
2023-04-06 14:13:45 +0200

    structure suite

---
+++ controller/publicController.php
@@ -0,0 +1,29 @@
+<?php
+
+if(isset($_GET['page'])){
+
+    switch($_GET['page']){
+        
+        case 'electrophone': 
+            include "../view/public_view/electrophone.php";
+            break;
+        
+        case 'idiophone':
+            include "../view/public_view/idiophone.php";
+            break;
+        
+        case 'membranophone':
+            include "../view/public_view/membranophone.php";
+            break;
+
+        case 'contact':
+            include "../view/public_view/contact.php";
+            break;
+
+    default:
+        include_once "../view/public_view/homePage.php";
+        break;
+    }
+}else{
+    include_once "../view/public_view/homePage.php";
+}
