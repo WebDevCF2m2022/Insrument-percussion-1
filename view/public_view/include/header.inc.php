7f8a8894500eb1980e61ac1da1032a9d4cd588b4
valerie2107 <kinkamyl@hotmail.com>
2023-04-14 11:23:54 +0200

    navbar en php

---
+++ view/public_view/include/header.inc.php
@@ -0,0 +1,27 @@
+<header>
+               
+        <div class="menu_toggle">
+            <span></span>
+        </div>
+        <div class="logo">
+            <p><span>Pro</span>jet</p> <!--nom sur la barre nav-->
+        </div>
+        <ul class="menu">
+            <li><a href="?page=homePage">Accueil</a></li>
+            <li><a href="?page=electrophone">Electrophone</a></li>
+            <li><a href="?page=idiophone">Idiophone</a></li>
+            <li><a href="?page=membranophone">Membranophone</a></li> 
+            <li><a href="?page=contact">Contact</a></li> 
+        </ul>
+        <nav class="navbar bg-light">
+            <div class="container-fluid">
+              <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </nav>
+        
+        <button class="login_btn">LOGIN</button> <!--cée le bouton -->
+        
+</header> 
